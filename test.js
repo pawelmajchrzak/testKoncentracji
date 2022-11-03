d60db714e6e796175d38fecb6364fb095b717de3
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2022-11-03 23:38:14 +0100

    implemetation clock

--- test.js
+++ test.js
@@ -1,4 +1,55 @@
+const ONE_SECOND = 1000;
 
+class Clock {
+    constructor() {
+        this.clock = null; // interval
+        this.currentTime = 0;
+        this.limitTime = 0;
+        this.$clock = document.querySelector('#panelGry');
+    }
+
+    start(time) {
+        console.log('start', time);
+        this.limitTime = time;
+        this.render();
+        this.clock = setInterval(() => {
+            this.currentTime += ONE_SECOND;
+            this.render();
+
+            if (this.isStopped()) {
+                this.stop();
+            }
+        }, ONE_SECOND);
+    }
+
+    isStopped() {
+        return (this.limitTime + this.currentTime) == 300000;
+    }
+
+    render() {
+        let diff = this.limitTime + this.currentTime;
+        console.log('render', diff);
+        this.$clock.textContent = Clock.formatTime(diff);
+    }
+
+    stop() {
+        console.log('stop');
+        clearInterval(this.clock);
+        }
+
+    static parseSeconds(formattedTime) {
+        let [minutes, seconds] = formattedTime.split(':').map(Number);
+        return (minutes * 60 * ONE_SECOND) + seconds * ONE_SECOND;
+    }
+
+    static formatTime(time) {
+        let minutes = Math.floor(time / ONE_SECOND / 60);
+        let seconds = time / ONE_SECOND % 60;
+        minutes = String(minutes).padStart(2, '0');
+        seconds = String(seconds).padStart(2, '0');
+        return `${minutes}:${seconds}`
+    }
+}
 
 
 function stworzPlansze(poziom)
@@ -32,6 +83,10 @@ function stworzPlansze(poziom)
 	
 	for (i=0; i<iloscKafelek; i++) liczby[i]++;
 	
+	let clock = new Clock();
+    let time = Clock.parseSeconds('00:00');
+    clock.start(time);
+
 	for (i=0; i<iloscKafelek; i++)
 	{
 		var nrkafelki = "kafNr"+i;
@@ -40,8 +95,7 @@ function stworzPlansze(poziom)
 	}
 	
 	$('#tablica').html(tresc_diva);
-	var ileSek = 10;
-	odliczanie(ileSek);
+
 }
 
 function sprawdz(nr,iloscKafelek)
@@ -81,26 +135,24 @@ function zakryjKafelek(nr,iloscKafelek)
 
 	if (liczby[nr]==iloscKafelek)
 	{
+		
 		$('#tablica').css('font-size','40px');
 		$('#tablica').html('<br /><b>Udało Ci się! Wygrałeś!</b><br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>');
+		
+		var element = document.getElementById('panelGry');
+		var zawartosc = 'Twój czas to:  '+element.textContent;
+		$('#test').html(zawartosc);
+		
+		Clock.stop();
 	}
 	liczby[nr]=100;
 	document.getElementById(nr).setAttribute("onclick",";");
 }
 
-function odliczanie(ileSek)
-{
 
-           document.getElementById("panelGry").innerHTML = ileSek;
-		   ileSek = ileSek-1;
 
-            if(liczba<0) ileSek = 10;
-            setTimeout("odliczanie(ileSek)",1000);
-        
-
-	
-
-	
-
+function setup() {
 
 }
+
+//window.onload = setup;
